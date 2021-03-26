38530faa1ccc52171ea7361fa4815df357f44d31
The man <ThisIsntTheWay@users.noreply.github.com>
2021-03-26 14:33:39 +0100

    Create utilities.h

---
+++ src/utilities.h
@@ -0,0 +1,12 @@
+/*
+    ESP32 Nixie Clock - Utilities module
+    (c) V. Klopfenstein, 2021
+
+    This code block contains various functions that cannot be categorized.
+*/
+
+
+#ifndef utils_h
+#define utils_h
+
+#endif
