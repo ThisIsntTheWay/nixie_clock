70883b7a963483e657f117da78712b937c748285
The man <ThisIsntTheWay@users.noreply.github.com>
2021-03-31 21:39:35 +0200

    Change pins

--- src/system/nixie.h
+++ src/system/nixie.h
@@ -18,9 +18,9 @@
 //  ---------------------
 
 // Bitshift pins
-#define DS_PIN 11   // Latch
-#define SH_CP 12    // Clock
-#define ST_CP 12    // Data
+#define DS_PIN 32   // Latch
+#define SH_CP 33    // Clock
+#define ST_CP 25    // Data
 
 byte data;
 
@@ -30,8 +30,8 @@ byte data;
 
 // The following function was inspired by:
 // https://forum.arduino.cc/index.php?topic=449828.msg3094698#msg3094698
-void displayNumber(int number_1, int number_2) {
-    byte n1, n2;
+void displayNumber(int number_1, int number_2) {//, int number_3, int number_4) {
+    byte n1, n2, n3, n4;
 
     switch (number_1) {
         case 0: n1 = 0b0000; break;
@@ -59,6 +59,32 @@ void displayNumber(int number_1, int number_2) {
         case 9: n2 = 0b1001; break;
     }
 
+    /*switch (number_3) {
+        case 0: n3 = 0b0000; break;
+        case 1: n3 = 0b1000; break;
+        case 2: n3 = 0b0100; break;
+        case 3: n3 = 0b1100; break;
+        case 4: n3 = 0b0010; break;
+        case 5: n3 = 0b1010; break;
+        case 6: n3 = 0b0110; break;
+        case 7: n3 = 0b1110; break;
+        case 8: n3 = 0b0001; break;
+        case 9: n3 = 0b1001; break;
+    }
+
+    switch (number_4) {
+        case 0: n4 = 0b0000; break;
+        case 1: n4 = 0b1000; break;
+        case 2: n4 = 0b0100; break;
+        case 3: n4 = 0b1100; break;
+        case 4: n4 = 0b0010; break;
+        case 5: n4 = 0b1010; break;
+        case 6: n4 = 0b0110; break;
+        case 7: n4 = 0b1110; break;
+        case 8: n4 = 0b0001; break;
+        case 9: n4 = 0b1001; break;
+    }*/
+
     // Push to shift registers
     digitalWrite(DS_PIN, LOW);
     shiftOut(ST_CP, SH_CP, LSBFIRST, (n1 << 4) | n2);
@@ -70,6 +96,7 @@ void displayNumber(int number_1, int number_2) {
 //  ---------------------
 
 void taskUpdateNixie(void* parameter) {
+    Serial.println(F("[T] Nixie: preparing nixie updater..."));
     pinMode(DS_PIN, OUTPUT);
 
     while (!RTCready) { vTaskDelay(1000); }
@@ -77,23 +104,29 @@ void taskUpdateNixie(void* parameter) {
     int lastMinute = now.minute();
     int lastHour = now.hour();
     
+    Serial.println(F("[T] Nixie: Spawning nixie updater..."));
     for (;;) {
+        Serial.print(F("[T] Nixie: lastHour: "));
+            Serial.println(lastHour);
+        Serial.print(F("[T] Nixie: lastMinute: "));
+            Serial.println(lastMinute);
         
         // Periodically display time
         if (lastMinute != now.minute()) {
             Serial.println(F("[T] Nixie: Updating minutes..."));
 
-            int lastMinute = now.minute();
+            lastMinute = now.minute();
             displayNumber(now.hour(), now.minute());
         } else if (lastHour != now.hour()) {
             Serial.println(F("[T] Nixie: Updating hours..."));
             
-            int lastHour = now.minute();
+            lastHour = now.minute();
             displayNumber(now.hour(), now.minute());
-
+        } else {
+            Serial.println(F("[T] Nixie: Idle..."));
         }
 
-        vTaskDelay(2000);
+        vTaskDelay(3000);
     }
 }
 
