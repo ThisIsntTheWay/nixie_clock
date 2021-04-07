9dc18be2259ec7d9e768033d548598e31824c583
The man <ThisIsntTheWay@users.noreply.github.com>
2021-04-07 23:28:37 +0200

    Implement basic factory reset

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,63 @@
 #include <system/nixie.h>
 #include <utils/sysInit.h>
 
+TaskHandle_t TaskRTC_Handle;
+TaskHandle_t TaskNixie_Handle;
+
+// https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2018/08/esp32-pinout-chip-ESP-WROOM-32.png
+// Factory reset button GPIO pin
+#define FACT_RST 17
+
+//  ---------------------
+//  FUNCTIONS
+//  ---------------------
+
+// Task to monitor if a factory reset has been triggered.
+void taskfactoryResetWDT(void* parameter) {
+    unsigned long lastMillis = millis();
+    unsigned long currMillis;
+
+    pinMode(FACT_RST, INPUT_PULLDOWN);
+
+    for (;;) {
+        // Check if factory reset button is pressed using some millis() math
+        currMillis = millis();
+        if (digitalRead(FACT_RST)) {
+            // Trigger factory reset if pin is HIGH for at least 5 seconds
+            if (currMillis > lastMillis + 5000) {
+                // Perform factory reset
+                Serial.println("[!!!] FACTORY RESET INITIATED [!!!]");
+
+                // Destroy all tasks
+                Serial.println("[!] Reset: Destroying perpetual tasks...");
+                vTaskDelete(TaskRTC_Handle);
+                vTaskDelete(TaskNixie_Handle);
+                server.end();
+
+                // Destroy config files
+                Serial.println("[!] Reset: Nuking /config dir...");
+                File configDir = LITTLEFS.open("/config");
+                File configFile = configDir.openNextFile();
+
+                while (configFile) {
+                    Serial.print(" > Destroying: ");
+                        Serial.println(configFile.name());
+                    
+                    LITTLEFS.remove(configFile.name());
+
+                    configFile = configDir.openNextFile();
+                }
+
+                // At last, restart ESP
+                Serial.println("[!] Reset: Restarting ESP...");
+                ESP.restart();
+            }
+        } else { lastMillis = millis(); }
+
+        vTaskDelay(100);
+    }
+}
+
 //  ---------------------
 //  MAIN
 //  ---------------------
@@ -23,13 +80,17 @@ void setup() {
 
     // FreeRTOS tasks
     Serial.println(F("[i] Spawning tasks..."));
-    xTaskCreate(taskWiFi, "WiFi initiator", 2048, NULL, 1, NULL);
-    xTaskCreate(taskFSMount, "FS Mount", 2500, NULL, 1, NULL);
-    xTaskCreate(taskSetupRTC, "RTC Setup", 3500, NULL, 1, NULL);
-    xTaskCreate(taskSetupHUE, "HUE Setup", 3500, NULL, 1, NULL);
 
-    xTaskCreate(taskUpdateRTC, "RTC Sync", 3500, NULL, 1, NULL);
-    xTaskCreate(taskUpdateNixie, "Nixie updater", 3500, NULL, 1, NULL);
+    // One-time / setup tasks
+        xTaskCreate(taskWiFi, "WiFi initiator", 2048, NULL, 1, NULL);
+        xTaskCreate(taskFSMount, "FS Mount", 2500, NULL, 1, NULL);
+        xTaskCreate(taskSetupRTC, "RTC Setup", 3500, NULL, 1, NULL);
+        xTaskCreate(taskSetupHUE, "HUE Setup", 3500, NULL, 1, NULL);
+
+    // Perpetual tasks
+        xTaskCreate(taskUpdateRTC, "RTC Sync", 3500, NULL, 1, &TaskRTC_Handle);
+        xTaskCreate(taskUpdateNixie, "Nixie updater", 3500, NULL, 1, &TaskNixie_Handle);
+        xTaskCreate(taskfactoryResetWDT, "FRST WDT", 1500, NULL, 1, NULL);
 
     webServerStartup();
 
