cfe6c913f0db9ff346f4d3602089818f0c5d1475
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-15 11:15:45 +0200

    Add comment for acquiring mklittlefs

--- replace_fs.py
+++ replace_fs.py
@@ -1,3 +1,6 @@
 Import("env")
 print("Replace MKSPIFFSTOOL with mklittlefs.exe")
 env.Replace (MKSPIFFSTOOL = "mklittlefs.exe")
+
+# Acquire mklittlefs.exe from here: https://github.com/earlephilhower/mklittlefs/releases
+# Place in root of project directory.
