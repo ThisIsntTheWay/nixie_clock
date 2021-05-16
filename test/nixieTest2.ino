646d8a4393abd9e2edb248d7edfc5faa99740496
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-16 20:32:59 +0200

    Update nixieTest2.ino

--- test/nixieTest2.ino
+++ test/nixieTest2.ino
@@ -1,65 +1,72 @@
 // Bitshift pins
-#define latchPin 19   // Latch  | ST_CP
-#define clockPin 18   // Clock  | SH_CP
-#define dataPin 23    // Data   | DS
+#define DS_PIN  27   // Data
+#define SH_CP   26   // Clock
+#define ST_CP   25   // Latch
 
-/*  a sketch to drive two nixie tubes using two SN74141 BCD chips and a single SN74HC595N shift register
-    developed from the tutorials on Adafruit.com and arduino.cc
-    the sketch will cause two nixie tubes to count from 0 to 99 but you can change it to create any two-digit number and have the nixie tube display it
-    Jeff Glans 2013
-    Released into the public domain
+
+int loopNum = 0;
+
+// Convert dec to BCD
+byte decToBCD(byte in) {
+    return( (in/16*10) + (in%16) );
+}
+
+// The following function was inspired by:
+// https://forum.arduino.cc/index.php?topic=449828.msg3094698#msg3094698
+void displayNumber(int number_1, int number_2, int number_3, int number_4) {
+    byte n1, n2, n3, n4;
     
-*/
-//set up the pins for communication with the shift register
-int x; //create a counting variable
+    Serial.print("Displaying the following numbers: ");
+        Serial.print(number_1);
+        Serial.print(" ");
+        Serial.print(number_2);
+        Serial.print(" ");
+        Serial.print(number_3);
+        Serial.print(" ");
+        Serial.println(number_4);
 
+    n1 = decToBCD(number_1);
+    n2 = decToBCD(number_2);
+    n3 = decToBCD(number_3);
+    n4 = decToBCD(number_4);
+    /*  Nixie display [Tube 2, hours]
+     *  0 = 2
+        1 = 3
+        2 = 2
+        3 = 3
+        4 = 6
+        5 = 7
+        6 = 6
+        7 = 7
+        8 = X
+        9 = X
+     */
 
-// create an array that translates decimal numbers into an appropriate byte for sending to the shift register
-int charTable[] = {
-0,1,2,3,4,5,6,7,8,9,
-16,17,18,19,20,21,22,23,24,25,
-32,33,34,35,36,37,38,39,40,41,
-48,49,50,51,52,53,54,55,56,57,
-64,65,66,67,68,69,70,71,72,73,
-80,81,82,83,84,85,86,87,88,89,
-96,97,98,99,100,101,102,103,104,105,
-112,113,114,115,116,117,118,119,120,121,
-128,129,130,131,132,133,134,135,136,137,
-144,145,146,147,148,149,150,151,152,153};
+    Serial.print(" > Num2 BCD: "); Serial.println(n2, BIN);
+    Serial.print(" > Pass 1 BCD: "); Serial.println((n3 << 4 | n4), BIN);
+    Serial.print(" > Pass 2 BCD: "); Serial.println((n1 << 4 | n2), BIN);
 
+    // Push to shift registers
+    digitalWrite(ST_CP, LOW);
+    shiftOut(DS_PIN, SH_CP, MSBFIRST, (n3 << 4) | n4);  // REG1 [Hours]
+    shiftOut(DS_PIN, SH_CP, MSBFIRST, (n1 << 4) | n2);  // REG2 [Minutes]
+    digitalWrite(ST_CP, HIGH);
+}
 
-byte nixies = 255; //initiate the byte to be sent to the shift register and set it to blank the nixies
-
-void setup(){
-  pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
-  pinMode(clockPin, OUTPUT);
+void setup() {
   Serial.begin(115200);
+
+  pinMode(DS_PIN, OUTPUT);
+  pinMode(SH_CP, OUTPUT);
+  pinMode(ST_CP, OUTPUT);
 }
 
-void loop(){
-  nixies = 255; // create a blank byte
-  updateShiftRegister(); // send the blank byte to the shift register
-  delay(500);
-  
+void loop() {
+  loopNum++;
+  Serial.println(loopNum);
 
-  for (x = 0; x<100; x++){ // count from 0 to 99
-    nixies = charTable[x]; // translate into a byte to send to the shift register
-     
-    updateShiftRegister(); //send to the shift register
-    delay(500);
-  
-  Serial.print("x = ");
-  Serial.println(x);
-  Serial.print("nixies = ");
-  Serial.println(nixies);}
+  for (int i = 0; i < 10; i++) {
+   displayNumber(i,i,i,i);
+   delay(2550);
+  }
 }
-  //the process of sending a byte to the shift register
-void updateShiftRegister(){
-  digitalWrite(latchPin, LOW);
-  shiftOut(dataPin, clockPin, MSBFIRST, nixies);
-  digitalWrite(latchPin, HIGH);
-}
-  
-
-
