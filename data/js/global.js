82b8edc2c0f97072a3e2a60155ad89899317582a
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-18 19:11:29 +0200

    Copy SSID by link tag

--- data/js/global.js
+++ data/js/global.js
@@ -19,6 +19,7 @@ function spinner() {
                 responseText.innerHTML = spinnerFrames[currFrame];
                 currFrame = (currFrame == spinnerFrames.length - 1) ? 0 : currFrame + 1;
             } else {
+                // Assume timeout, hence showing an error
                 responseText.innerHTML = "&#x274C;&#x23F1;"
             }
         } else {
@@ -33,6 +34,7 @@ function tableFromJson(jsonTable, mode) {
     data = JSON.parse(jsonTable);
     var table = "<table>";
 
+    let i = 0;
     if (mode == 0) {
         table += 
             `<tr>
@@ -44,22 +46,22 @@ function tableFromJson(jsonTable, mode) {
             </tr>`;
 
         for (var key in data) {
-            console.log("Key" + key);
             table += `
                 <tr>
-                    <td>${data[key]["ssid"]}</td>
+                    <td><a href="#" id="ssid${i}" value="${data[key]["ssid"]}" onclick="copySelfToElement(this, 'wifi_client_ssid')">${data[key]["ssid"]}</a></td>
                     <td>${data[key]["mac"]}</td>
                     <td>${data[key]["channel"]}</td>
                     <td>${data[key]["rssi"]}</td>
                     <td>${data[key]["security"]}</td>
                 </tr>`;
+            i++;
         }
         table += "</table>";
     } else if (mode == 1) {
         for (var key in data) {
             table += 
             `<tr>
-                <td>${key}</td>
+                <th>${key}</th>
                 <td>${data[key]}</td>
             </tr>`;
         }
@@ -68,4 +70,8 @@ function tableFromJson(jsonTable, mode) {
 
     // Now, add the newly created table with json data, to a container.
     responseText.innerHTML = table;
+}
+
+function copySelfToElement(caller, element) {
+    document.getElementById(element).value = document.getElementById(caller.id).getAttribute("value");
 }
