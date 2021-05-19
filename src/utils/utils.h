8decee1622970247d208e154eef34bdd966e30e3
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-20 00:25:20 +0200

    Change connection failure text

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,7 +6,7 @@
 String getSysMsg() {
     String msg;
 
-    if (APisFallback) msg = "<span style='color: red'>Attempting to connect to '" + parseNetConfig(4) + "' has failed.</span>";
+    if (APisFallback) msg = "<span style='color: red'>Attempting to connect to WiFi network '" + parseNetConfig(4) + "' has failed.</span>";
 
     return msg;
 }
