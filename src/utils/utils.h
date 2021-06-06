931eadfaef7e8a9770bd829f7bb4422604b62b4b
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-06 17:17:23 +0200

    Update utils.h

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -11,21 +11,21 @@ String getSysMsg() {
     String msg = "";
     bool isError = false;
 
-    if (!globalErrorOverride) {
-        if (APisFallback) {
-            msg = "Could not connect to WiFi network '" + parseNetConfig(4) + "'."; isError = true;
-        } else if (!NTPisValid) {
-            msg = "NTP server is unresponsive: '" + parseRTCconfig(1) + "'."; isError = true;
-        } else {
+    if (APisFallback) {
+        msg = "Could not connect to WiFi network '" + parseNetConfig(4) + "'."; isError = true;
+    } else if (!NTPisValid) {
+        msg = "NTP server is unresponsive: '" + parseRTCconfig(1) + "'."; isError = true;
+    } else {
+        if (!globalErrorOverride) {
             switch (rtc_get_reset_reason(0)) {
                 case 12 : msg = "System rebooted due to core panic."; isError = true; break;
                 case 15 : msg = "Brownout detector triggered; power supply possibly unstable."; isError = true; break;
                 default : msg = "";
             }
         }
-
-        if (isError) msg = "<span style='color: red'>" + msg + "</span>";
     }
+
+    if (isError) msg = "<span style='color: red'>" + msg + "</span>";
 
     return msg;
 }
