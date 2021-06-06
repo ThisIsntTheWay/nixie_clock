85a4434ba7c2d492bdd3ae2ecaba9590007a8395
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-06 16:23:50 +0200

    Add more verbose error reporting

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,12 +1,28 @@
 #include <utils/network.h>
+#include "rom/rtc.h"
 
 #ifndef utils_h
 #define utils_h
 
+void getESPresetReason(RESET_REASON reason)
+{
+
+}
+
 String getSysMsg() {
     String msg;
-    if (APisFallback) msg = "<span style='color: red'>Attempting to connect to WiFi network '" + parseNetConfig(4) + "' has failed.</span>";
+    bool isError = false;
 
+    if (APisFallback) {
+        msg = "Could not connect to WiFi betwork '" + parseNetConfig(4) + "'."; isError = true;
+    } else {
+        switch (rtc_get_reset_reason(0)) {
+            case 15 : msg = "Brownout detection triggered! Power supply possibly unstable."; isError = true; break;/**<15, Reset when the vdd voltage is not stable*/
+            default : msg = "No reason";
+        }
+    }
+
+    if (isError) msg = "<span style='color: red'>" + msg + "</span>";
     return msg;
 }
 
