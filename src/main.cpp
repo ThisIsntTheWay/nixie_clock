5832e353ceca35fd4492e94a354be4e5cc7fe81d
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-16 14:16:48 +0200

    Regress classes implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,81 @@
     ESP32 Nixie Clock - Entrypoint
     (c) V. Klopfenstein, 2021
 
-    This is the entrypoint of the system.
+    This is the entrypoint of the whole program.
+    The purpose of this file is to spawn all tasks built with FreeRTOS.
 */
 
 #include "Arduino.h"
 
-#include <system/tasks.h>
-#include <system/webserver.h>
-#include <utils/sysInit.h>
+// Custom headers
+#include <system/webServer.h>
+#include <system/rtc.h>
+#include <system/nixie.h>
+#include <utils/network.h>
+
+TaskHandle_t TaskRTC_Handle;
+TaskHandle_t TaskNixie_Handle;
+TaskHandle_t TaskBright_Handle;
+TaskHandle_t TaskHUE_Handle;
+
+// https://i0.wp.com/randomnerdtutorials.com/wp-content/uploads/2018/08/esp32-pinout-chip-ESP-WROOM-32.png
+// Factory reset button GPIO pin
+#define FACT_RST 17
+
+// Task to monitor if a factory reset has been triggered.
+void taskfactoryResetWDT(void* parameter) {
+    unsigned long activeMillis;
+    unsigned long currMillis;
+
+    pinMode(FACT_RST, INPUT_PULLDOWN);
+
+    for (;;) {
+        vTaskDelete(NULL);
+
+        // Check if factory reset button is pressed using some millis() math
+        currMillis = millis();
+        if (digitalRead(FACT_RST)) activeMillis = currMillis;
+
+        // Trigger factory reset if pin is HIGH for at least 5 seconds
+        if ((currMillis - activeMillis >= 5000) || EnforceFactoryReset) {
+            // Perform factory reset
+            Serial.println("[!!!] FACTORY RESET INITIATED [!!!]");
+
+            while (!FlashFSready) {
+                Serial.println("[!] Reset: Awaiting FS mount...");
+                vTaskDelay(500);
+            }
+
+            // Destroy all tasks
+            Serial.println("[!] Reset: Destroying perpetual tasks...");
+            vTaskDelete(TaskRTC_Handle);
+            vTaskDelete(TaskNixie_Handle);
+            vTaskDelete(TaskHUE_Handle);
+
+            server.end();
+
+            // Destroy config files
+            Serial.println("[!] Reset: Nuking /config dir...");
+            File configDir = LITTLEFS.open("/config");
+            File configFile = configDir.openNextFile();
+
+            while (configFile) {
+                Serial.print(" > Destroying: ");
+                    Serial.println(configFile.name());
+                
+                LITTLEFS.remove(configFile.name());
+
+                configFile = configDir.openNextFile();
+            }
+
+            // At last, restart ESP
+            Serial.println("[!] Reset: Restarting ESP...");
+            ESP.restart();
+        }
+    }
+
+    vTaskDelay(100);
+}
 
 //  ---------------------
 //  MAIN
@@ -17,17 +84,6 @@
 
 void setup() {
     Serial.begin(115200);
-
-    // Init booleans
-    RTCready = false;
-    NTPisValid = true;
-    APisFallback = false;
-    
-    nixieSetupComplete = false;
-    nixieAutonomous = true;
-    forceUpdate = true;
-    cycleNixies = true;
-    crypto = false;
 
     Serial.println(F("[i] System booting up..."));
     Serial.printf("[i] Previous reset reason: Core 0: %d, Core 1: %d\n", rtc_get_reset_reason(0), rtc_get_reset_reason(1));
@@ -37,6 +93,17 @@ void setup() {
     Serial.println(F("[i] Conducting an initial WiFi scan..."));
     WiFi.scanNetworks();
     Serial.println(F("[i] Initial WiFi scan complete."));
+
+    // Shift registers
+    pinMode(DS_PIN, OUTPUT);
+    pinMode(SH_CP, OUTPUT);
+    pinMode(ST_CP, OUTPUT);
+
+    // Clear nixies
+    digitalWrite(ST_CP, LOW);
+    shiftOut(DS_PIN, SH_CP, MSBFIRST, 0b11111111);
+    shiftOut(DS_PIN, SH_CP, MSBFIRST, 0b11111111);
+    digitalWrite(ST_CP, HIGH);
 
     // FreeRTOS tasks
     Serial.println(F("[i] Starting tasks..."));
