3da235d4ad16f739e2a77f20ad357ccde5c431e3
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-20 12:30:54 +0200

    Remove artifacts from branch merge

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -8,20 +8,18 @@
 #ifndef utils_h
 #define utils_h
 
-#include <utils/sysInit.h>
+#include <utils/network.h>
 #include "rom/rtc.h"
 
 bool globalErrorOverride = false;
 bool bigError = false;
-
-SysInit sI;
 
 String getSysMsg() {
     String msg = "";
     bool isError = false;
 
     if (APisFallback && !bigError) {
-        msg = "Could not connect to WiFi network '" + sI.parseNetConfig(4) + "'."; isError = true;
+        msg = "Could not connect to WiFi network '" + parseNetConfig(4) + "'."; isError = true;
     } else if (!NTPisValid && !bigError) {
         msg = "NTP server is unresponsive."; isError = true;
     } else {
