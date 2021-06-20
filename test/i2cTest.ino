2987f451f5661769e9eaba1a1b5b9ae4f36e829d
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-20 10:09:21 +0200

    Create i2cTest.ino

---
+++ test/i2cTest.ino
@@ -0,0 +1,50 @@
+// Basically just the I2C scanner sketch: https://playground.arduino.cc/Main/I2cScanner/
+
+#include <Wire.h>
+#include <Arduino.h>
+
+void setup() {
+  Wire.begin();
+ 
+  Serial.begin(115200);
+}
+ 
+ 
+void loop() {
+  byte error, address;
+  int nDevices;
+ 
+  Serial.println("Scanning...");
+ 
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+ 
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+
+      if (address<16)
+        Serial.print("0");
+
+      Serial.print(address,HEX);
+      Serial.println("  !");
+ 
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknown error at address 0x");
+
+      if (address<16)
+        Serial.print("0");
+
+      Serial.println(address,HEX);
+    }    
+  } if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+ 
+  delay(750);           // wait 5 seconds for next scan
+}
